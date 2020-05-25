346a18c10acd78642f70fd57b8f42f9ff256b512
waqas saleem <waqas.mba86@gmail.com>
2020-05-25 15:21:10 +0500

    additions in products page

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
+
+
+  function order(){
+    
+    alert("Congratulations ..! Your order has been confirmed, We will delivered your order on your registered address within 20 minitus");
+  }
